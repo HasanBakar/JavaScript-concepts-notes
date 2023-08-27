8386a0f20afd23e34293d2a84044565575e3d67e
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2023-08-28 03:41:52 +0600

    json concepts

---
+++ JS Miscellaneous/json_concept.js
@@ -0,0 +1,38 @@
+// ! Introduction to JSON 
+/** 
+ --------------------------
+Javascript and JSON
+---------------------------
+*** JSON stands for JavaScript Object Notation. JSON is a text-based data format that is used to store and transfer data.
+*** In JSON, the data are in key/value pairs separated by a comma.
+JSON was derivative from javascript.
+
+The JSON syntax resembles the javascript object literal syntax
+
+****                                                                          ****
+    The JSON format can be accessed and be created by other programming languages
+****                                                                          ****
+
+ */
+
+//! JSON Data
+/*
+*** JSON data consist of key/value pairs similar to JavaScript object properties. The key and value are written in double quotes separated by a colon : example
+"name":"Abu Bakar"
+*/
+//* JSON data requires double quotes for the key
+{
+    "name": "Abu Bakar",
+    "age":24,
+    "status":true,
+}
+
+
+//! Javascript object and JSON
+
+/*
+JSON	                                            JavaScript Object
+The key in key/value pair should be in double quotes.	****    The key in key/value pair can be without double quotes.
+JSON cannot contain functions.    ****  	JavaScript objects can contain functions.
+JSON can be created and used by other programming languages. **** 	JavaScript objects can only be used in JavaScript.
+*/
